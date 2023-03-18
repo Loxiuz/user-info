6ace69903051a6a588bfc974bee86f0bc24eda1f
Malte <maltebjoerklund@gmail.com>
2023-03-18 20:12:20 +0100

    opgave færdiggjort

--- app.js
+++ app.js
@@ -3,22 +3,42 @@
 window.addEventListener("load", initApp);
 
 function initApp() {
-    // Event Listeners
-    document.querySelector("#form-create").addEventListener("submit", createUser);
+  // Event Listeners
+  document.querySelector("#form-create").addEventListener("submit", createUser);
 }
 
 function createUser(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const name = this.name.value;
-    const title = this.title.value;
-    const mail = this.mail.value;
-    const image = this.image.value;
+  const name = this.name.value;
+  const userName = this.userName.value;
+  const age = this.age.value;
+  const title = this.title.value;
+  const mail = this.mail.value;
+  const image = this.image.value;
 
-    console.log(name);
-    console.log(title);
-    console.log(mail);
-    console.log(image);
+  console.log(name);
+  console.log(userName);
+  console.log(age);
+  console.log(title);
+  console.log(mail);
+  console.log(image);
 
-    // validate user info
+  console.log(validateUser(name, userName, age, title, mail, image));
+
+  // validate user info
+}
+
+function validateUser(name, userName, age, title, mail, image) {
+  if (
+    name.length > 2 &&
+    title &&
+    ((mail.length > 6 && mail.length < 20) || userName) &&
+    image &&
+    age >= 18
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
